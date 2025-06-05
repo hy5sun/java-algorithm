4525d0b141df5c841586af3f2c809d8608789c02
Hyosun <84787653+hy5sun@users.noreply.github.com>
2025-06-05 19:26:06 +0900

    [Silver III] Title: N과 M (1), Time: 1628 ms, Memory: 61988 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class Main {
     private static int N, M;
+    private static int[] ch, nums;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,24 +12,27 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        List<Integer> nums = new ArrayList<>();
+        ch = new int[N+1];
+        nums = new int[M];
 
-        getPermutation(1, nums);
+        getPermutation(1);
 
     }
 
-    private static void getPermutation(int L, List<Integer> nums) {
+    private static void getPermutation(int L) {
         if (L == M+1) {
-            for (int n : nums)
-                System.out.print(n+ " ");
+            for (int n : nums) {
+                System.out.print(n + " ");
+            }
             System.out.println();
         }
         else {
             for (int i = 1; i <= N; i++) {
-                if (!nums.contains(i)) {
-                    nums.add(i);
-                    getPermutation(L + 1, nums);
-                    nums.remove(nums.size() - 1);
+                if (ch[i] == 0) {
+                    ch[i] = 1;
+                    nums[L-1] = i;
+                    getPermutation(L + 1);
+                    ch[i] = 0;
                 }
             }
         }
