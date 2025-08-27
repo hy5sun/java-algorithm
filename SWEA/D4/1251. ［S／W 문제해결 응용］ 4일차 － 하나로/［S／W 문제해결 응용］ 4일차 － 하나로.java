fff285a79e45d55acf729fee532116200ed53105
Hyosun <84787653+hy5sun@users.noreply.github.com>
2025-08-27 10:40:56 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 1,571 ms, Memory: 109,576 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -4,7 +4,6 @@ import java.io.*;
 /**
  * SW1251. 하나로
  * 
- * 
  * [문제]
  * - N개의 섬들을 연결.
  * - 환경 부담 세율 (E) * 해저 터널 길이 (L) ^ 2
@@ -17,23 +16,19 @@ import java.io.*;
  *  - 각 섬의 Y좌표
  *  - 해저터널 건설의 환경 부담 세율 실수 E
  * 
- * 2. 간선별 환경 부담금을 저장한다.
+ * 2. 인접 행렬에 간선별 환경 부담금을 저장한다.
  * 
- * 3. 2번에서 만든 거 오름차순 정렬
+ * 3. 첫 번째 섬에서부터 시작한다.
+ *  - 첫 번째 섬을 큐에 넣는다.
+ *  - 첫 번째 섬을 방문 처리한다.
+ *  - 첫 번째 섬과 연결되어 있는 간선을 조회한다.
+ *  - 연결되어 있는 섬을 우선순위 큐에 추가한다.
+ *  - 또 꺼내서 방문 여부 확인하고, 방문 처리하고, 비용값에 더하고, 연결되어 있는 섬을 조회하는 과정을 큐가 빌 때까지 반복한다.
  * 
- * 4. rank, parent 배열 초기화
- * 
- * 5. 2번에서 저장한 배열을 하나씩 돌려보는데,
- *  - 2개의 섬에 대해 부모가 동일한지 확인한다.
- *      - 동일하면 pass
- *      - 동일하지 않으면
- *          - union 작업하기
- *          - 비용 더하기
- * 
- * 6. 최소 비용을 출력한다.
+ * 4. 최소 비용을 출력한다.
  */
 public class Solution {
-        static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
 
@@ -41,10 +36,24 @@ public class Solution {
     static int[][] islandLocation;
     static double tax;
     static double[][] costs;
-
-    static int[] parent;
-    static int[] rank;
     static double totalTax;
+
+    static class Node {
+        int islandIdx;
+        double cost;
+
+        public Node(int islandIdx, double cost) {
+            this.islandIdx = islandIdx;
+            this.cost = cost;
+        }
+    }
+
+    static class NodeComparator implements Comparator<Node> {
+        @Override
+        public int compare(Node n1, Node n2) {
+            return Double.compare(n1.cost, n2.cost);
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         // 1. 입력 받기
@@ -56,7 +65,8 @@ public class Solution {
             islandCnt = Integer.parseInt(br.readLine());
 
             islandLocation = new int[islandCnt][2];
-            costs = new double[islandCnt * (islandCnt - 1) / 2][3];
+
+            costs = new double[islandCnt][islandCnt];
 
             // - 각 섬의 X좌표
             st = new StringTokenizer(br.readLine().trim());
@@ -71,84 +81,63 @@ public class Solution {
             // - 해저터널 건설의 환경 부담 세율 실수 E
             tax = Double.parseDouble(br.readLine());
 
-            // 2. 간선별 환경 부담금을 저장한다.
-            int costIdx = 0;
-
+            // 2. 인접 행렬에 간선별 환경 부담금을 저장한다.
             for (int idx = 0; idx < islandCnt-1; idx++) {
                 for (int nextIdx = idx + 1; nextIdx < islandCnt; nextIdx++) {
-                    costs[costIdx][0] = idx;
-                    costs[costIdx][1] = nextIdx;
-
                     double length = getLength(idx, nextIdx);
-                    costs[costIdx][2] = calculateTax(length);
+                    double cost = calculateTax(length);
 
-                    costIdx++;
-                }   
+                    costs[idx][nextIdx] = cost;
+                    costs[nextIdx][idx] = cost;
+                }
             }
-
-            // 3. 2번에서 만든 거 오름차순 정렬
-            Arrays.sort(costs, Comparator.comparingDouble(c -> c[2]));
 
             totalTax = 0;
-            
-            // 4. rank, parent 배열 초기화
-            make();
 
-            // 5. 2번에서 저장한 배열을 하나씩 돌려보는데,
-            // - 2개의 섬에 대해 부모가 동일한지 확인한다.
-            //     - 동일하면 pass
-            //     - 동일하지 않으면
-            //         - union 작업하기
-            //         - 비용 더하기
-            for (double[] cost : costs) {
-                int costAParent = find((int) cost[0]);
-                int costBParent = find((int) cost[1]);
-
-                if (costAParent == costBParent) continue;
-
-                union((int) cost[0], (int)cost[1]);
-
-                totalTax += cost[2];
-            }
-
-            totalTax = Math.round(totalTax);
+            // 3. 첫 번째 섬에서부터 시작한다.
+            connect(0);
 
             sb.append("#").append(testCase).append(" ").append(Math.round(totalTax)).append("\n");
         }
 
-        //  6. 최소 비용을 출력한다.
+        //  4. 최소 비용을 출력한다.
         System.out.println(sb.toString());
     }
 
-    private static void make() {
-        parent = new int[islandCnt];
-        rank = new int[islandCnt];
+    // 3. 첫 번째 섬에서부터 시작한다.
+    // *  - 첫 번째 섬을 큐에 넣는다.
+    // *  - 첫 번째 섬을 방문처리한다.
+    // *  - 첫 번째 섬과 연결되어 있는 간선을 조회한다.
+    // *  - 연결되어 있는 섬을 우선순위 큐에 추가한다.
+    // *  - 또 꺼내서 방문 여부 확인하고, 방문 처리하고, 비용값에 더하고, 연결되어 있는 섬을 조회하는 과정을 큐가 빌 때까지 반복한다.
+    private static void connect(int islandIdx) {
+        PriorityQueue<Node> queue = new PriorityQueue<>(new NodeComparator());
+        boolean[] visited = new boolean[islandCnt];
 
-        for (int idx = 0; idx < islandCnt; idx++)
-            parent[idx] = idx;
-    }
+        visited[islandIdx] = true;
 
-    private static int find(int element) {
-        if (element == parent[element]) return element;
-
-        return parent[element] = find(parent[element]);
-    }
-
-    private static void union(int elementA, int elementB) {
-        int parentA = find(elementA);
-        int parentB = find(elementB);
-
-        if (parentA == parentB) return;
-
-        if (rank[parentA] > rank[parentB]) {
-            parent[parentB] = parentA;
-            return;
+        for (int nextIsland = 0; nextIsland < islandCnt; nextIsland++) {
+            if (costs[islandIdx][nextIsland] != 0) {
+                queue.offer(new Node(nextIsland, costs[islandIdx][nextIsland]));
+            }
         }
 
-        parent[parentA] = parentB;
+        while (!queue.isEmpty()) {
+            Node nextisland = queue.poll();
 
-        if (rank[parentA] == rank[parentB])
-            rank[parentB]++;
+            int currentIdx = nextisland.islandIdx;
+
+            if (visited[currentIdx]) continue;
+
+            visited[currentIdx] = true;
+            totalTax += nextisland.cost;
+
+            for (int nextIsland = 0; nextIsland < islandCnt; nextIsland++) {
+                if (costs[currentIdx][nextIsland] != 0) {
+                    queue.offer(new Node(nextIsland, costs[currentIdx][nextIsland]));
+                }
+            }
+        }
     }
 
     // 환경 부담 세율 (E) * 해저 터널 길이 (L) ^ 2
