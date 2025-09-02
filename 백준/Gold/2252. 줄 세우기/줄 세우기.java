f2526df9cdfa480c71913df2459664c584043fc2
Hyosun <84787653+hy5sun@users.noreply.github.com>
2025-09-02 16:31:12 +0900

    [Gold III] Title: 줄 세우기, Time: 396 ms, Memory: 47892 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,109 +1,80 @@
 import java.io.*;
+import java.io.InputStreamReader;
 import java.util.*;
 
 /**
- * BJ2252. 줄 세우기
- *
- * [문제]
- * - N명의 학생들을 키 순서대로 줄을 세운다.
- * - 일부 학생들의 키만 비교해서 정렬한다.
- * - 입력으로 A, B의 학생 키가 주어지는데, A가 B 앞에 서야 한다는 뜻이다.
- * - 학생들 번호는 1 ~ N 번이다.
- *
- * [풀이]
- * 1. 입력 받기
- *  - 학생 수, 비교 수를 입력 받는다.
- *  - 비교 수만큼 학생 키 순서를 입력 받는다. (A B -> A > B)
- *      - 정점별로 자신을 향하고 있는 정점의 개수를 저장한다. : connectedVertexCnt
- *      - 정점별로 자신이 향하고 있는 정점들을 인접 리스트로 저장한다. : toVertexes
- *
- * 2. BFS를 통해 위상정렬한다.
- *  - 자신을 향하고 있는 정점의 개수가 0인 애들을 queue에 넣어준다.
- *  - queue가 빌 때까지 다음을 반복한다.
- *      - queue 맨 앞 정점을 꺼낸다.
- *      - sortedStudents에 추가한다.
- *      - 맨 앞 정점이 향하고 있는 정점들을 조회한다.
- *      - connectedVertexCnt[정점]--; 를 한다.
- *      - connectedVertexCnt[정점] == 0 이라면 queue에 추가한다.
- *
- * 3. 출력한다.
+ * 위상정렬.. 
+ * 
+ * 위상정렬 진행하기
+ * - 자신 앞에 있어야 하는 정점의 개수가 0인 애들을 모두 Queue에 넣는다.
+ * - Queue가 빌 때까지 다음을 반복한다.
+ * 	- Queue의 맨 앞 정점을 꺼낸다.
+ * 	- 출력문에 포함시킨다.
+ * 	- 해당 정점의 앞에 있어야 하는 정점들을 조회한다.
+ * 		- 조회한 정점의 (앞에 있어야 하는 정점 개수) 를 1 빼준다.
+ * 		- 조회한 정점의 (앞에 있어야 하는 정점 개수) == 0이면 queue에 추가한다.
  */
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static StringBuilder sortedStudents = new StringBuilder();
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	
+	static int studentCnt, compareCnt;
+	static List<List<Integer>> compare = new ArrayList<>();
+	static int[] count;
+	
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine().trim());
+		
+		studentCnt = Integer.parseInt(st.nextToken());
+		compareCnt = Integer.parseInt(st.nextToken());
+		
+		count = new int[studentCnt+1];
+		
+		for (int idx = 0; idx <= studentCnt; idx++)
+			compare.add(new ArrayList<>());
+		
+		for (int idx = 0; idx < compareCnt; idx++) {
+			st = new StringTokenizer(br.readLine().trim());
+			
+			int studentA = Integer.parseInt(st.nextToken());
+			int studentB = Integer.parseInt(st.nextToken());
+			
+			compare.get(studentA).add(studentB); // 내 뒤에 와야 하는 놈들 저장
+			count[studentB]++; // 내 앞에 와야 하는 놈들의 수
+		}
+		
+		bfs();
+		
+		System.out.println(sb.toString());
+	}
+	
+	private static void bfs() {
+		Queue<Integer> queue = new LinkedList<>();
+		
+		for (int studentIdx = 1; studentIdx <= studentCnt; studentIdx++) {
+			if (count[studentIdx] == 0) // 내 앞에 올 애들이 아무도 ㄷ없다?
+				queue.add(studentIdx);
+		}
+		
+		while (!queue.isEmpty()) {
+			int currentStudentIdx = queue.poll();
+			
+			sb.append(currentStudentIdx).append(" ");
+			
+			// 나는 넣었으니, 내 뒤에 있기로 한 애들 넣어줘야지
+			List<Integer> behindMe = compare.get(currentStudentIdx);
+			
+			for (int behindMeStudentIdx : behindMe) {
+				
+				// 니 앞에 있어야 하는 내가 사라졌으니까 1빼주기
+				count[behindMeStudentIdx]--;
+				
+				if (count[behindMeStudentIdx] == 0)
+					queue.offer(behindMeStudentIdx);
+			}
+			
+		}
+	}
 
-    static int studentCnt, compareCnt;
-    static List<List<Integer>> toVertexes = new ArrayList<>();
-    static int[] connectedVertexCnt;
-
-    public static void main(String[] args) throws IOException {
-        // 1. 입력 받기
-        // *  - 학생 수, 비교 수를 입력 받는다.
-        st = new StringTokenizer(br.readLine().trim());
-
-        studentCnt = Integer.parseInt(st.nextToken());
-        compareCnt = Integer.parseInt(st.nextToken());
-
-        connectedVertexCnt = new int[studentCnt + 1];
-
-        for (int idx = 0; idx <= studentCnt; idx++) {
-            toVertexes.add(new ArrayList<>());
-        }
-
-        // - 비교 수만큼 학생 키 순서를 입력 받는다. (A B -> A > B)
-        //        - 정점별로 자신을 향하고 있는 정점의 개수를 저장한다. : connectedVertexCnt
-        //        - 정점별로 자신이 향하고 있는 정점들을 인접 리스트로 저장한다. : toVertexes
-        for (int idx = 0; idx < compareCnt; idx++) {
-            st = new StringTokenizer(br.readLine().trim());
-
-            int studentA = Integer.parseInt(st.nextToken());
-            int studentB = Integer.parseInt(st.nextToken());
-
-            toVertexes.get(studentA).add(studentB);
-            connectedVertexCnt[studentB]++;
-        }
-
-        // 2. BFS를 통해 위상정렬한다.
-        sortStudents();
-
-        // 3. 출력한다.
-        System.out.println(sortedStudents.toString());
-    }
-
-    /**
-     * BFS를 통해 위상정렬한다.
-     *  *  - 자신을 향하고 있는 정점의 개수가 0인 애들을 queue에 넣어준다.
-     *  *  - queue가 빌 때까지 다음을 반복한다.
-     *  *      - queue 맨 앞 정점을 꺼낸다.
-     *  *      - sortedStudents에 추가한다.
-     *  *      - 맨 앞 정점이 향하고 있는 정점들을 조회한다.
-     *  *      - connectedVertexCnt[정점]--; 를 한다.
-     *  *      - connectedVertexCnt[정점] == 0 이라면 queue에 추가한다.
-     */
-    private static void sortStudents() {
-        Queue<Integer> queue = new LinkedList<>();
-
-        for (int studentIdx = 1; studentIdx <= studentCnt; studentIdx++) {
-            if (connectedVertexCnt[studentIdx] == 0) {
-                queue.offer(studentIdx);
-            }
-        }
-
-        while (!queue.isEmpty()) {
-            int currentStudent = queue.poll();
-
-            sortedStudents.append(currentStudent).append(" ");
-
-            List<Integer> toVertex = toVertexes.get(currentStudent);
-
-            for (int vertexIdx : toVertex) {
-                connectedVertexCnt[vertexIdx]--;
-
-                if (connectedVertexCnt[vertexIdx] == 0) {
-                    queue.offer(vertexIdx);
-                }
-            }
-        }
-    }
 }
